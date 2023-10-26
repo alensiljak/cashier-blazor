9a60fa953159e07c194da79b8585053759ba6992
Alen Šiljak <dev@alensiljak.eu.org>
2023-10-26 23:36:08 +0200

    header styling

---
+++ src/Cashier/Components/Pages/DataDemo.razor.cs
@@ -0,0 +1,17 @@
+﻿using BlazorDexie.JsModule;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Cashier.Components.Pages
+{
+    public partial class DataDemo
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; } = null!;
+        protected override async Task OnInitializedAsync()
+        {
+            var moduleFactory = new EsModuleFactory(JSRuntime);
+
+        }
+    }
+}
