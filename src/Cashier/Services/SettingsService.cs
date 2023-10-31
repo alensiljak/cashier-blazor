a264d13bc6b23890386fa9f49a99da96d491b986
Alen Šiljak <dev@alensiljak.eu.org>
2023-10-31 21:54:24 +0100

    saving the currency setting

--- src/Cashier/Services/SettingsService.cs
+++ src/Cashier/Services/SettingsService.cs
@@ -1,14 +1,41 @@
-﻿namespace Cashier.Services
+﻿using DexieNET;
+using DexieNET.Component;
+using Cashier.Domain;
+
+namespace Cashier.Services
 {
     // Operations for application settings.
     public class SettingsService
     {
-        public string get(string key)
+        private CashierDB _db;
+
+        public SettingsService(CashierDB db) {
+            _db = db;
+        }
+
+        public async Task<string> Get(string key)
         {
             // todo
             // fetch data
+            //var settings = _db.Settings().ToArray();
+            var record = await _db.Settings().Get(key);
+            if (record == null)
+            {
+                await _db.Settings().Add(new Setting(key, string.Empty));
+                return string.Empty;
+            }
+            else
+            {
+                return record.Value;
+            }
+        }
 
-            return null;
+        public async Task<bool> Set(string key, string value)
+        {
+            // _db.Open()
+            // return await _db.Settings().Update(key, () => new Setting(key, value), value);
+            var setting = new Setting(key, value);
+            return await _db.Settings().Update(key, setting => setting.Value,  value);
         }
     }
 }
