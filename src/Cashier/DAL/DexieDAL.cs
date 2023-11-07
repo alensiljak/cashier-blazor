361fb6ca30a493760f3ee8f8865930bb3375d3d0
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-07 21:31:58 +0100

    adding dexie-blazor

---
+++ src/Cashier/DAL/DexieDAL.cs
@@ -0,0 +1,15 @@
+﻿using BlazorDexie.Database;
+using BlazorDexie.JsModule;
+using Cashier.Model;
+
+namespace Cashier.DAL
+{
+    public class DexieDAL : Db
+    {
+        public Store<Setting, string> Settings { get; set; } = new(nameof(Setting.Key), nameof(Setting.Value));
+
+        public DexieDAL(IModuleFactory moduleFactory)
+            : base("Cashier", 1, new DbVersion[] {}, moduleFactory)
+        { }
+    }
+}
---
+++ src/Cashier/Model/DexieModel.cs
@@ -0,0 +1,15 @@
+﻿/*
+ */
+namespace Cashier.Model
+{
+    public partial record SettingRecord(
+        string Key,
+        string Value
+    );
+
+    public class Setting
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
