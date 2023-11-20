77dd2f57cf9044f4c053cb23b55f6e3286c5da1a
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-20 18:35:10 +0100

    version

--- src/Cashier/Lib/Constants.cs
+++ src/Cashier/Lib/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.20.1800";
+        public static string VersionNumber = "2023.11.20.1835";
 
         public static string AssetAllocationFilename = "asset-allocation.toml";
     }
