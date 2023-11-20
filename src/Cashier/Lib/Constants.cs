effc07e576b6895e32c38b5fac7208f1c2139986
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-20 19:03:18 +0100

    autofocus on search; error handling on server shutdown

--- src/Cashier/Lib/Constants.cs
+++ src/Cashier/Lib/Constants.cs
@@ -2,8 +2,9 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.20.1835";
+        public static string VersionNumber = "2023.11.20.1903";
 
         public static string AssetAllocationFilename = "asset-allocation.toml";
+        public static string DefaultFabPosition = "position: fixed; bottom: 30px; right: 25px;";
     }
 }
