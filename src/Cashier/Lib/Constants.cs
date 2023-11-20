a574a73ebd1ff4c0500b8b6c08d988525c7fdc61
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-20 23:24:31 +0100

    upload doesn't work

--- src/Cashier/Lib/Constants.cs
+++ src/Cashier/Lib/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.20.1903";
+        public static string VersionNumber = "2023.11.20.2323";
 
         public static string AssetAllocationFilename = "asset-allocation.toml";
         public static string DefaultFabPosition = "position: fixed; bottom: 30px; right: 20px;";
