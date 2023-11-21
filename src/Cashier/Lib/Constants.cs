4f3875cbd8599e920ab0fcd107b93b92c8214ebf
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-21 10:38:41 +0100

    preparing to load asset class detail

--- src/Cashier/Lib/Constants.cs
+++ src/Cashier/Lib/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.21.0927";
+        public static string VersionNumber = "2023.11.21.1030";
 
         public static string AssetAllocationFilename = "asset-allocation.toml";
         public static string DefaultFabPosition = "position: fixed; bottom: 30px; right: 20px;";
