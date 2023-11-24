05f84a7b907178ad48eb7ffe15b468691e1b3639
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-24 21:41:49 +0100

    version

--- src/Cashier/Data/Constants.cs
+++ src/Cashier/Data/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.24.1200";
+        public static string VersionNumber = "2023.11.24.2141";
 
         public static string AssetAllocationFilename = "asset-allocation.toml";
         public static string DefaultFabPosition = "position: fixed; bottom: 30px; right: 20px;";
