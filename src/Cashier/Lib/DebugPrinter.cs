411fc6625e7334328314791016b4c72b0791ade7
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-24 12:00:03 +0100

    payees list; debugging saving

---
+++ src/Cashier/Lib/DebugPrinter.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Cashier.Lib
+{
+    /// <summary>
+    /// Prints the given object into the Console.
+    /// </summary>
+    public class DebugPrinter
+    {
+        [UnconditionalSuppressMessage("Trimming", "IL2026:Members annotated with 'RequiresUnreferencedCodeAttribute' require dynamic access otherwise can break functionality when trimming application code", Justification = "<Pending>")]
+        public static void Print(object obj)
+        {
+            foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+            {
+                string name = descriptor.Name;
+                object? value = descriptor.GetValue(obj);
+                Console.WriteLine("{0} = {1}", name, value);
+            }
+        }
+
+        public static void PrintJson(object obj)
+        {
+            var output = JsonConvert.SerializeObject(obj);
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ src/Cashier/Services/RouterService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+
+namespace Cashier.Services
+{
+    /// <summary>
+    /// Some manual handling for route navigation
+    /// </summary>
+    public class RouterService
+    {
+        private IJSRuntime _runtime;
+
+        public RouterService(IJSRuntime runtime) { 
+            _runtime = runtime;
+        }
+
+        public async Task Back()
+        {
+            await _runtime.InvokeVoidAsync("history.back");
+        }
+    }
+}
