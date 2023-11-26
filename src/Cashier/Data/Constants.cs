dd45a5aada492cb05501f8522b4ddda6151a86ff
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-26 18:33:27 +0100

    coloring the xacts in the journal card

--- src/Cashier/Data/Constants.cs
+++ src/Cashier/Data/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.26.1816";
+        public static string VersionNumber = "2023.11.26.1830";
 
         public static string AssetAllocationFilename = "asset-allocation.toml";
         public static string DefaultFabPosition = "position: fixed; bottom: 30px; right: 20px;";
