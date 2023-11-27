715ae54ec671efcdf45c7d608cff3d03d8b0d445
Alen Šiljak <dev@alensiljak.eu.org>
2023-11-27 11:34:28 +0100

    removing explicit version numbers

--- src/Cashier/Data/Constants.cs
+++ src/Cashier/Data/Constants.cs
@@ -2,8 +2,6 @@
 {
     public static class Constants
     {
-        public static string VersionNumber = "2023.11.26.1830";
-
         public static string AssetAllocationFilename = "asset-allocation.toml";
         public static string DefaultFabPosition = "position: fixed; bottom: 30px; right: 20px;";
         public static string ISODateFormat = "yyyy-MM-dd";
