ccb444c3ee154a2d7f2cbc5b5f7ad81b43c446e0
Alen Šiljak <dev@alensiljak.eu.org>
2024-01-01 16:57:08 +0100

    making netlify local instead of global

--- src/Cashier/publish.cmd
+++ src/Cashier/publish.cmd
@@ -8,4 +8,4 @@ dotnet publish -c Release
 ::popd
 
 :: Deploy
-netlify deploy --prod --dir=bin/Release/net8.0/publish/wwwroot --message="Publishing to Prod"
+npx netlify deploy --prod --dir=bin/Release/net8.0/publish/wwwroot --message="Publishing to Prod"
