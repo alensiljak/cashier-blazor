a16bf77a70d0570d7b6e3ccbaa80575c9e9c9887
Alen Šiljak <dev@alensiljak.eu.org>
2024-06-05 12:52:43 +0200

    ideas for the schedule projection

---
+++ src/Cashier.Tests/UnitTests/SchedulerTests.cs
@@ -0,0 +1,34 @@
+﻿using Cashier.Lib;
+using Cashier.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cashier.Tests.UnitTests
+{
+    public class SchedulerTests
+    {
+        private Scheduler _scheduler;
+
+        public SchedulerTests() { 
+            _scheduler = new Scheduler();
+        }
+
+        [Fact]
+        public void TestProjection()
+        {
+            var scX = new ScheduledXact
+            {
+                NextDate = DateOnly.FromDateTime(DateTime.Parse("2024-05-01"))
+            };
+            var endDate = DateOnly.FromDateTime(DateTime.Parse("2024-08-30"));
+
+            _scheduler.ProjectSchedule(scX, endDate);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ src/Cashier/Lib/Scheduler.cs
@@ -0,0 +1,27 @@
+﻿using Cashier.Model;
+
+namespace Cashier.Lib
+{
+    /// <summary>
+    /// Business logic for schedules (i.e. scheduling a transaction).
+    /// </summary>
+    public class Scheduler
+    {
+        /// <summary>
+        /// Project the schedule in the period start date - end date.
+        /// For example, if the schedule repeats daily and the end date is in a week's time,
+        /// the result should be 7 dates/occurrences.
+        /// </summary>
+        /// <param name="scheduledXact">Scheduled Transaction with the schedule information.</param>
+        /// <param name="startDate">The start date for the projection period.</param>
+        /// <param name="endDate">The end date of the projection period.</param>
+        public List<DateOnly> ProjectSchedule(ScheduledXact scheduledXact, DateOnly startDate, DateOnly endDate)
+        {
+            var result = new List<DateOnly>();
+
+            if (scheduledXact.NextDate > endDate) return result;
+
+            throw new NotImplementedException();
+        }
+    }
+}
